4d374b588c5e6f1833bebd9d67efdbb80254009a
parksrazor <pakjkwan@gmail.com>
2020-06-26 10:58:20 +0900

    10:58

--- src/main/java/com/parksrazor/web/controllers/HomeController.java
+++ src/main/java/com/parksrazor/web/controllers/HomeController.java
@@ -14,20 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@GetMapping("/")
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		model.addAttribute("serverTime", 
-				DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale)
-				.format(new Date()) );
+	public String home() {
+		logger.info("홈컨트롤러 진입");
+	
 		return "Home.jsp";
 	}
 	
